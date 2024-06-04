b569fccc578b9f80da97a1cfb6b90e5430cdf75c
job zablon <zablonjob@gmail.com>
2024-06-04 02:44:32 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Importing requests module
+"""
 
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    # Construct the URL for subreddit information
-    sub_info_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-    # Set custom User-Agent header
-    headers = {"User-Agent": "Ubuntu 20.04"}
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    all_data = response.json()
 
-    # Make the GET request to the subreddit information URL
-    response = requests.get(
-        sub_info_url, headers=headers, allow_redirects=False)
+    try:
+        return all_data.get('data').get('subscribers')
 
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    return 0
+    except:
+        return 0
