c5bdbe696755c3019867853812160ef6717ed23f
job zablon <zablonjob@gmail.com>
2024-06-04 02:36:22 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Functions to query subscribers on a given Reddit subreddit."""
+"""Module for task 0"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+
+    # Construct the URL for subreddit information
+    sub_info_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Set custom User-Agent header
+    headers = {"User-Agent": "Ubuntu 20.04"}
+
+    # Make the GET request to the subreddit information URL
+    response = requests.get(
+        sub_info_url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""
-Using the reddit's API
-"""
+""" task 2 """
+
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
-    """returns top 10 post titles recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+def recurse(subreddit, hot_list=[], after=None):
+    """Recursive function that queries the Reddit
+    API and returns a list of titles"""
 
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'custom'}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 200:
+        r = r.json()
+        for post in r.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if r.get('data').get('after'):
             recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
-        return (None)
+        return None
